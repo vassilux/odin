8e40a8857ea0200ceb2638d8b9902c73827c82f3
vassilux <v.gontcharov@gmail.com>
2014-03-27 17:23:05 +0100

    Create package

--- prepare_deploy.sh
+++ prepare_deploy.sh
@@ -17,8 +17,6 @@ if [ -d "$DEPLOY_DIR" ]; then
     rm -rf  "$DEPLOY_DIR"
 fi
 #
-rm -rf  ./pyodin/logs/*
-#
 mkdir "$DEPLOY_DIR"
 #
 cp -aR client/app "$DEPLOY_DIR"
@@ -26,16 +24,32 @@ cp -aR install "$DEPLOY_DIR"
 cp -aR pyodin "$DEPLOY_DIR"
 cp -aR server "$DEPLOY_DIR"
 
-rm -rf "$DEPLOY_DIR/pyodin/logs/*"
-rm -rf "$DEPLOY_DIR/pyodin/conf/*.conf"
+rm -rf "$DEPLOY_DIR/pyodin/logs/*.log"
+rm -rf "$DEPLOY_DIR/pyodin/conf/odinami.conf"
+rm -rf "$DEPLOY_DIR/pyodin/conf/odinamilogger.conf"
+rm -rf "$DEPLOY_DIR/pyodin/conf/odinf1com.conf"
+rm -rf "$DEPLOY_DIR/pyodin/conf/odinincall.conf"
+rm -rf "$DEPLOY_DIR/pyodin/conf/odinsys.conf"
+rm -rf "$DEPLOY_DIR/pyodin/conf/odinsyslogger.conf"
 rm -rf "$DEPLOY_DIR/pyodin/test"
+
+rm -rf "$DEPLOY_DIR/server/test"
+rm -rf "$DEPLOY_DIR/server/logs/*.log"
+rm -rf "$DEPLOY_DIR/server/config/config.json"
+rm -rf "$DEPLOY_DIR/server/config.js"
+rm -rf "$DEPLOY_DIR/server/.gitignore"
+rm -rf "$DEPLOY_DIR/server/.travis.yml"
+rm -rf "$DEPLOY_DIR/server/gruntFile.js"
+rm -rf "$DEPLOY_DIR/server/generatePackages.js"
+rm -rf "$DEPLOY_DIR/server/Makefile"
+rm -rf "$DEPLOY_DIR/server/TODO.txt"
 
 mkdir "$DEPLOY_DIR/docs"
 cp -aR README.pdf "$DEPLOY_DIR/docs"
 
-find ${DEPLOY_DIR} -name CVS -prune -exec rm -rf {} \;
-find ${DEPLOY_DIR} -name .svn -prune -exec rm -rf {} \;
-find ${DEPLOY_DIR} -name .pyc -prune -exec rm -rf {} \;
+find ${DEPLOY_DIR}/* -name CVS -prune -exec rm -rf {} \;
+find ${DEPLOY_DIR}/* -name .svn -prune -exec rm -rf {} \;
+find ${DEPLOY_DIR}/* -name .pyc -prune -exec rm -rf {} \;
 
 DEPLOY_FILE_NAME="odin_${VER_MAJOR}_${VER_MINOR}_${VER_PATCH}_${VER_FLAG}".tar.gz
 tar cvzf "$DEPLOY_FILE_NAME" "$DEPLOY_DIR"
